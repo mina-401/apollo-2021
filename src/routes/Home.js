723f63e0de46d3f8111fc5eba5e4cd2c672d610f
mina-401 <fgz1234@naver.com>
2021-08-29 17:29:07 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,16 @@
-export default ()=>"Home";
+import React from "react";
+import { useQuery,gql } from "@apollo/client";
+
+const GET_MOVIES=gql`
+    {
+        movies{
+            id
+            title
+        }
+    }
+`
+export default () => {
+    const {loading,data}=useQuery(GET_MOVIES);
+    console.log(loading);
+    return <h1>Home</h1>;
+}
